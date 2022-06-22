113c21c6a4b9e64c382c967a8e5353ade0df6309
Gulam Ansari <gulamansari57181@gmail.com>
2022-06-22 18:46:12 +0530

    Implemented some string methods

---
+++ StringMethods.js
@@ -0,0 +1,28 @@
+//In javascript string is an immutable objects
+
+let myName = "Mohd Gulam Ansari";
+
+//string length is a property
+
+console.log(myName.length);
+
+/* The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
+
+The substring() method extracts characters from start to end (exclusive).
+
+The substring() method does not change the original string.
+
+If start is greater than end, arguments are swapped: (4, 1) = (1, 4).
+
+Start or end values less than 0, are treated as 0. */
+
+console.log(myName.substring(0));
+
+console.log(myName.substring(0, 6));
+
+console.log(myName.substring(6, 0));
+
+console.log(myName.substring(0, 12));
+
+//negative argyment traeted as 0
+console.log(myName.substring(-3));
---
+++ StringPad.js
@@ -0,0 +1,14 @@
+//String.padStart() is used to add expected amount of numbers or characters in start of the string
+
+/* The padStart() method pads the current string with another string 
+(multiple times, if needed) until the resulting string reaches the given length. 
+The padding is applied from the start of the current string.
+ */
+const ACCOUNT_NO = "58528286565";
+//to make Account no 16 digit we can add extra zeros at start
+console.log(ACCOUNT_NO.padStart(16, "*"));
+//output : ****58528286565
+
+//to make Account no 16 digit we can add extra zeros at end
+//Output : 58528286565****
+console.log(ACCOUNT_NO.padEnd(16, "*"));
